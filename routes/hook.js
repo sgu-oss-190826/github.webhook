6f36c6f29c6544012584af613916d3d3ce18b16f
Junyoung Lee <junyoung.plum@gmail.com>
2019-08-21 23:13:19 +0900

    opened PR event 수신 시 slack 메시지 발송

--- routes/hook.js
+++ routes/hook.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var exec = require("child_process").exec;
 var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  console.log('GitHub webhook incoming.', req.body);
+  let pr = req.body.pull_request;
+    
+  if(pr && req.body.action === 'opened') {
+    let content = {
+        text: `Pull Request가 도착했습니다\n${pr.html_url}`
+    };
+
+    let url = process.env.SLACK_INCOMING;
+    console.log('to slack', url);
+
+    let curl = `curl -X POST -H 'Content-type: application/json' --data '${JSON.stringify(content)}' ${url}`
+
+    exec(curl, function (error, stdout, stderr) {
+        console.log(stderr);
+      });
+  }
+
+  res.send('{}');
 });
 
 module.exports = router;
